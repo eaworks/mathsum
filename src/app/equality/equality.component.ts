0a1a2952a236f523ab08a0e29bb29f85cb451de1
Ersin Aydin <ersinaydinworks@gmail.com>
2022-12-22 11:38:39 +0300

    form implement

--- src/app/equality/equality.component.ts
+++ src/app/equality/equality.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-equality',
@@ -6,10 +7,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./equality.component.scss']
 })
 export class EqualityComponent implements OnInit {
+  mathForm = new FormGroup({
+    firstNumber: new FormControl(this.generateNumber()),
+    secondNumber: new FormControl(this.generateNumber()),
+    answer: new FormControl(''),
+  });
+  get firstNumber() {
+    return this.mathForm.value.firstNumber;
+  }
+  get secondNumber() {
+    return this.mathForm.value.secondNumber;
+  }
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  generateNumber() {
+    return Math.floor(Math.random() * 10);
   }
 
 }
